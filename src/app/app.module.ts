3a1962954874f865479eb3d372dcd5b3f0149920
Oscar Gonzalez <imoskgn@gmail.com>
2021-11-11 19:43:15 -0500

    Adding Home and Update Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './partials/header/header.component';
+import { UpdateComponent } from './pages/update/update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
