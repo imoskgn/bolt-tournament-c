99564aca3a0f404cd897d067e878800bbd3c2f60
Shahrzad Kolahdooz <shahrzad.kolahdooz@gmail.com>
2021-12-01 11:22:57 +0330

    login redirect fixed

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -14,7 +15,7 @@ export class AuthService {
     headers: new HttpHeaders(this.headerDict),
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private route: Router) { }
 
   //User login function
   userLogin(payload: any) {
@@ -27,6 +28,7 @@ export class AuthService {
           console.log(res);
           localStorage.setItem('jwt',res.accessToken);
           alert("Login Successful");
+          this.route.navigate(['/landing']).then.call(window.location.reload());
        // }
       }
     );
