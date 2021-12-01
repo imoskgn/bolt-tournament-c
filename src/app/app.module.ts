10e0ea11f38de77160d5400645bc54d22fd866df
Shahrzad Kolahdooz <shahrzad.kolahdooz@gmail.com>
2021-12-01 10:12:23 +0330

    organized components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './partials/header/header.component';
-import { UpdateComponent } from './pages/update/update.component';
+import { UpdateComponent } from './tournament/update/update.component';
 import { FooterComponent } from './partials/footer/footer.component';
 import { BasePageComponent } from './partials/base-page/base-page.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { CreateComponent } from './pages/create/create.component';
+import { CreateComponent } from './tournament/create/create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
-import { DetailComponent } from './pages/detail/detail.component';
-import { DeleteComponent } from './pages/delete/delete.component';
+import { DetailComponent } from './tournament/detail/detail.component';
+import { DeleteComponent } from './tournament/delete/delete.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { AuthInterceptor } from './auth.interceptor';
-import { ForumCreateComponent } from './pages/forum-create/forum-create.component';
-import { ForumMainComponent } from './pages/forum-main/forum-main.component';
-import { ForumDetailComponent } from './pages/forum-detail/forum-detail.component';
+import { ForumCreateComponent } from './forum/forum-create/forum-create.component';
+import { ForumMainComponent } from './forum/forum-main/forum-main.component';
+import { ForumDetailComponent } from './forum/forum-detail/forum-detail.component';
+import { UserInfoComponent } from './pages/user-info/user-info.component';
 
 
 
@@ -42,7 +43,8 @@ import { ForumDetailComponent } from './pages/forum-detail/forum-detail.componen
     LandingComponent,
     ForumMainComponent,
     ForumCreateComponent,
-    ForumDetailComponent
+    ForumDetailComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
