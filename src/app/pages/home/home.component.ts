a612db4566f7e2c860569fef8a9174f5be9b401f
AdrianaAriza <55102581+AdrianaAriza@users.noreply.github.com>
2021-12-07 12:55:19 -0500

    Fix: permissions to start, update and delete a tournament

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -30,7 +30,12 @@ export class HomeComponent implements OnInit {
   }
 
 
-  startTournament( t : Tournament): void{     
+  startTournament( t : Tournament): void{    
+    if(!localStorage.getItem('jwt')){
+      alert("To start a tournament you should be log in. \n Redirecting ....")
+      this.router.navigate(['/login'])
+      return;
+    } 
     let playerName : any | undefined;
     playerName = t.playersList;
     for (let i = 0; i < playerName.length; i++) {
@@ -42,5 +47,21 @@ export class HomeComponent implements OnInit {
         this.dbService.createMatch(t)
       }
     }
+  }
+  deleteTournament( tournament : Tournament): void{     
+    if(!localStorage.getItem('jwt')){
+      alert("To perform a delete you should be log in. \n Redirecting ....")
+      this.router.navigate(['/login'])
+      return;
+    }
+    this.router.navigate(['/delete/', tournament._id])
+  }
+  updateTournament( tournament : Tournament): void{     
+    if(!localStorage.getItem('jwt')){
+      alert("To perform an update you should be log in. \n Redirecting ....")
+      this.router.navigate(['/login'])
+      return;
+    }
+    this.router.navigate(['/update/', tournament._id])
   }
 }
